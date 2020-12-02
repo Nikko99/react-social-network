a36a8255897a52a63ce2e5c49dbca9194eaad0a0
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2020-12-02 16:17:34 +0300

    Dialogs props

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogsItem = (props) => {
+   return (
+      <div className={s.dialogs_item}>
+         <NavLink to={'/im/' + props.id} activeClassName={s.activeDialog}>{props.name}</NavLink>
+      </div>
+   )
+}
+
+const MessagesItem = (props) => {
+   return <div>{props.message}</div>
+}
 
 const Dialogs = () => {
    return (
       <div className={s.dialogs_wrapper}>
-         <div className={s.dialogs_list}>
-            <div className={s.dialogs_item + ' ' + s.active}>Akhmed</div>
-            <div className={s.dialogs_item}>Said</div>
-            <div className={s.dialogs_item}>Umalat</div>
+         <div>
+            <DialogsItem name='Akhmed' id='1'/>
+            <DialogsItem name='Said' id='2'/>
+            <DialogsItem name='Umalat' id='3'/>
          </div>
-         <div className={s.messages_list}>
-            <div className={s.messages_item}>Hello</div>
-            <div className={s.messages_item}>How are you?</div>
-            <div className={s.messages_item}>Okay</div>
+         <div>
+            <MessagesItem message='Hello'/>
+            <MessagesItem message='Its me'/>
+            <MessagesItem message='isadakdlasdnfajf'/>
          </div>
       </div>
    )
