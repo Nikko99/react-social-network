9add3b84825dbf92bfbe0a473b50fcb79c205149
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2020-12-04 20:54:46 +0300

    Добавил аватарки диалогам

--- src/components/Dialogs/DialogsItem/DialogsItem.jsx
+++ src/components/Dialogs/DialogsItem/DialogsItem.jsx
@@ -5,6 +5,7 @@ import {NavLink} from "react-router-dom";
 const DialogsItem = (props) => {
    return (
       <div className={s.dialogs_item}>
+         <img src="https://sun9-34.userapi.com/c623116/v623116626/46a18/ULJZYvSsER0.jpg?ava=1" alt=""/>
          <NavLink to={'/im/' + props.id} activeClassName={s.activeDialog}>{props.name}</NavLink>
       </div>
    )
