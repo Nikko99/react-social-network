aa6ca037031fd939d521d2161d63a6b81a9d37c3
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2020-12-13 20:59:35 +0300

    users api, npm axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,44 +1,14 @@
 import React from 'react';
-import s from './Users.module.css'
+import s from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/image/userPhoto.jpg'
 
 const Users = (props) => {
-
    if (props.users.length === 0) {
-      props.setUsers([
-         {
-            id: 1,
-            fullName: 'Akhmed',
-            userPhoto: 'https://sun9-34.userapi.com/c623116/v623116626/46a18/ULJZYvSsER0.jpg?ava=1',
-            followed: true,
-            status: 'I/m react developer',
-            location: {
-               country: 'Russia',
-               city: 'Rostov-on-Don'
-            }
-         },
-         {
-            id: 2,
-            fullName: 'Umalat',
-            userPhoto: 'https://sun9-34.userapi.com/c623116/v623116626/46a18/ULJZYvSsER0.jpg?ava=1',
-            followed: false,
-            status: 'I/m react developer',
-            location: {
-               country: 'Russia',
-               city: 'Rostov-on-Don'
-            }
-         },
-         {
-            id: 3,
-            fullName: 'Said',
-            userPhoto: 'https://sun9-34.userapi.com/c623116/v623116626/46a18/ULJZYvSsER0.jpg?ava=1',
-            followed: false,
-            status: 'I/m react developer',
-            location: {
-               country: 'Russia',
-               city: 'Karabulak'
-            }
-         }
-      ])
+      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+         .then(response => {
+            props.setUsers(response.data.items);
+         })
    }
 
    return (
@@ -46,18 +16,21 @@ const Users = (props) => {
          {
             props.users.map(u => {
                return (
-                  <div className={s.user_card}>
+                  <div key={ u.id } className={ s.user_card }>
                      <div>
-                        <img src={u.userPhoto} alt=""/>
+                        <img src={ u.photos.small !== null ? u.photos.small : userPhoto } alt=""/>
                      </div>
                      <div>
-                        <p>{u.fullName}</p>
-                        <p>{u.status}</p>
-                        <p>{u.location.country}, {u.location.city}</p>
+                        <p>{ u.name }</p>
+                        <p>{ u.status }</p>
 
-                        {u.followed
-                           ? <button onClick={ ()=>{props.unfollow(u.id)} }>Unfollow</button>
-                           : <button onClick={ ()=>{props.follow(u.id)} }>Follow</button>
+                        { u.followed
+                           ? <button onClick={ () => {
+                              props.unfollow(u.id)
+                           } }>Unfollow</button>
+                           : <button onClick={ () => {
+                              props.follow(u.id)
+                           } }>Follow</button>
                         }
                      </div>
                   </div>
